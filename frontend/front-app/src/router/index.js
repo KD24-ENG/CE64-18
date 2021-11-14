6d875ae9a8d719966a713f8b5fe3c03edf604d4e
KD24-ENG <62015005@kmitl.ac.th>
2021-11-14 23:04:42 +0700

    edit code

--- frontend/front-app/src/router/index.js
+++ frontend/front-app/src/router/index.js
@@ -1,30 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-import Home from '../views/Home.vue'
-import Secret from '../views/Secret.vue'
-import Login from '../views/Login.vue'
-import Register from '../views/Register.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: login
-  },
-  {
-    path: '/register',
-    name: 'register',
-    component: Register
-  },
-  {
-    path: '/secret',
-    name: 'secret',
-    component: Secret
   },
   {
     path: '/about',
