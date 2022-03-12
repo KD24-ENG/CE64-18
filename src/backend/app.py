3b4cb7aa0ee4fd719f7db97c722a91c997b308c3
KD24-ENG <62015005@kmitl.ac.th>
2022-03-12 15:21:12 +0700

    Back End code add

--- src/backend/app.py
+++ src/backend/app.py
@@ -4,10 +4,24 @@ from flask_cors import CORS
 import folium
 import datetime
 
+import datetime
+import matplotlib.pyplot as plt
+
+x = ['Mon Sep 1 16:40:20 2015', 'Mon Sep 1 16:45:20 2015',
+     'Mon Sep 1 16:50:20 2015', 'Mon Sep 1 16:55:20 2015']
+y = range(4)
+
+x = [datetime.datetime.strptime(elem, '%a %b %d %H:%M:%S %Y') for elem in x]
+
+(fig, ax) = plt.subplots(1, 1)
+ax.plot(x, y)
+fig.show()
+
 app = Flask(__name__)
 app.config.from_object(__name__)
 
 CORS(app, resource={r"/*": {"origins": "*"}})
+
 
 @app.route("/")
 def base():
@@ -16,8 +30,8 @@ def base():
         location=[13.756331, 100.501762],
         zoom_start=15
     )
-    
-    #folium.Polyline(
+
+    # folium.Polyline(
     #    location=[13.81226, 100.56266],
     #    color="red",
     #    weight=4).add_to(map)
@@ -27,12 +41,14 @@ def base():
         popup="<b>Marker here</b>",
         tooltip="Click Here!"
     ).add_to(map)
-    
+
     return map._repr_html_()
+
 
 @app.route("/test", methods=["GET", "POST"])
 def test():
     return "test test"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
