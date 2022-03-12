f4f0a6a574e1a066cf96d4ed9b21d3393c9c91fe
KD24-ENG <62015005@kmitl.ac.th>
2022-03-12 15:24:56 +0700

    Front End End code add

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -7,6 +7,39 @@
   </v-app>
 </template>
 
+<template>
+<l-map style="height: 350px" :zoom="zoom" :center="center">
+<l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+<l-polyline :lat-lngs="polyline.latlngs" :color="polyline.color"></l-polyline>
+</l-map>
+</template>
+
+<script>
+import {LMap, LTileLayer, LPolyline} from 'vue2-leaflet';
+
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+    LPolyline
+  },
+  data () {
+    return {
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      attribution:
+        '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      zoom: 8,
+      center: [47.313220, -1.319482],
+      polyline: {
+        latlngs: [[47.334852, -1.509485], [47.342596, -1.328731], [47.241487, -1.190568], [47.234787, -1.358337]],
+        color: 'green'
+      }
+    };
+  }
+}
+</script>
+
+
 <script>
 import Navbar from '@/components/Navbar.vue'
 
